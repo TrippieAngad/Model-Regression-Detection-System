6856cabda074e04dec7808ea1773b24563ff6c46
angad <angadbajwa096@gmail.com>
2026-07-02 14:03:16 -0400

    succesfully turned gemini payload into interpretable json payload using pydantic

--- app/customer_support_tool.py
+++ app/customer_support_tool.py
@@ -4,11 +4,17 @@ import yaml as y
 from pathlib import Path
 from dotenv import load_dotenv
 from google import genai as gemini
+import pydantic
+from pydantic import BaseModel
+from typing import Literal
 
 load_dotenv()
 GEMINI_KEY = os.getenv("GEMINI_KEY")
 client = gemini.Client(api_key =GEMINI_KEY )
 
+class EmailClassification(BaseModel):
+    category: Literal["billing", "technical", "academics", "spam"]
+    summary:str
 
 def email_classifier(email:str)-> json:
     BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +26,10 @@ def email_classifier(email:str)-> json:
     prompt_to_send = prompt_config["prompt"] 
     model_to_use = prompt_config["model_used"]
     
-    response = client.models.generate_content(model = model_to_use, contents = f"prompt: {prompt_to_send}. Email instance to use: {email}")
-    return response.text
-
+    response = client.models.generate_content(model = model_to_use, contents = f"prompt: {prompt_to_send}. Email instance to use: {email}").text
+    json_data = json.loads(response)
+    email_c = EmailClassification.model_validate(json_data)
+    return email_c.model_dump()
 
 print(email_classifier("I was charged twice for my subscription."))
 
